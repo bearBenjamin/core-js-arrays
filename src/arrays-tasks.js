b74fbd37351ebb78b7eb34ec81ac664a324bda84
Ilia Konev <122528494+bearBenjamin@users.noreply.github.com>
2025-10-13 14:09:56 +0300

    feat: add sum 1 task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -20,8 +20,12 @@
  *    getIntervalArray(0, 100) => [ 0, 1, 2, ..., 100 ]
  *    getIntervalArray(3, 3) => [ 3 ]
  */
-function getIntervalArray(/* start, end */) {
-  throw new Error('Not implemented');
+function getIntervalArray(start, end) {
+  const arr = [];
+  for (let i = start; i < end + 1; i += 1) {
+    arr.push(i);
+  }
+  return arr;
 }
 
 /**
